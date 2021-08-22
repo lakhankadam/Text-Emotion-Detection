1bd9908db5579b846e09779d0201edbeacec50c8
lakhankadam1 <lakhankadam1@gmail.com>
2021-08-22 14:34:52 +0530

    Create ngrams and features

---
+++ create_feature.py
@@ -0,0 +1,19 @@
+import re
+from collections import Counter
+
+def ngram(token, n): 
+    output = []
+    for i in range(n-1, len(token)): 
+        ngram = ' '.join(token[i-n+1:i+1])
+        output.append(ngram) 
+    return output
+
+def create_feature(text, nrange=(1, 1)):
+    text_features = [] 
+    text = text.lower() 
+    text_alphanum = re.sub('[^a-z0-9#]', ' ', text)
+    for n in range(nrange[0], nrange[1]+1): 
+        text_features += ngram(text_alphanum.split(), n)    
+    text_punc = re.sub('[a-z0-9]', ' ', text)
+    text_features += ngram(text_punc.split(), 1)
+    return Counter(text_features)
